e36e8c9d6cc25f24ccd4dcca1cc06fea516182dc
lifei07 <lifei07@kuaishou.com>
2025-04-18 05:50:41 +0000

    add print

--- mma_mem_latency.py
+++ mma_mem_latency.py
@@ -13,8 +13,8 @@ l2_throughput_per_cycle = 4472.3 # byte # float
 l2_throughput_per_sm_cycle = l2_throughput_per_cycle / 132
 
 # deepgemm fp8
-BLOCK_M = [128, 256]
-BLOCK_N = [128, 144, 160]
+BLOCK_M = [64, 128, 256]
+BLOCK_N = [64, 128, 144, 160]
 BLOCK_K = 128
 
 for m in BLOCK_M:
@@ -22,3 +22,16 @@ for m in BLOCK_M:
         fma_cycle = m * n  // sm90_fma_per_sm_cycle
         l2_cycle = (m + n) // l2_throughput_per_sm_cycle
         print(f"{m=} {n=} {fma_cycle/l2_cycle}")
+
+# m=64 n=64 0.6666666666666666
+# m=64 n=128 0.8
+# m=64 n=144 0.6666666666666666
+# m=64 n=160 0.8333333333333334
+# m=128 n=64 0.8
+# m=128 n=128 1.1428571428571428
+# m=128 n=144 1.125
+# m=128 n=160 1.25
+# m=256 n=64 0.8888888888888888
+# m=256 n=128 1.4545454545454546
+# m=256 n=144 1.6363636363636365
+# m=256 n=160 1.6666666666666667
